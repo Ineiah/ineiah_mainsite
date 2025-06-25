be590622822d50627d7bd8ce84658934775b1616
Zadigo <pendenquejohn@gmail.com>
2025-06-25 18:30:42 +0200

    Up packages and  improve tests

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vitest/config'
+// import path from 'path'
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'nuxt',
-    // environmentOptions: {
-    //   nuxt: {
-    //     rootDir: './'
-    //   }
-    // }
-  }
+    testTimeout: 1000
+    // projects: []
+  },
+  // resolve: {
+  //   alias: {
+  //     'firebase/app': path.resolve(__dirname, 'tests/mocks/firebase/app.ts'),
+  //     'firebase/database': path.resolve(__dirname, 'tests/mocks/firebase/database.ts')
+  //   }
+  // }
 })

6b5a54f371a0d33d58613609aee4b4d631353608
Zadigo <pendenquejohn@gmail.com>
2025-12-06 11:55:43 +0100

    Remove darkmode

--- frontend/app/composables/index.ts
+++ frontend/app/composables/index.ts
@@ -76,28 +76,28 @@ export const useDevComposable = createGlobalState(() => {
 export const useDarkModeComposable = createGlobalState(() => {
   const [darkMode, toggleDarkMode] = useToggle()
 
-  if (import.meta.client) {
-    const colorMode = useColorMode({
-      initialValue: 'light',
-      onChanged(mode, defaultHandler) {
-        if (mode == 'dark') {
-          document.documentElement.classList.add('p-dark')
-          defaultHandler(mode)
-        } else {
-          document.documentElement.classList.remove('p-dark')
-          defaultHandler(mode)
-        }
-      }
-    })
+  // if (import.meta.client) {
+  //   const colorMode = useColorMode({
+  //     initialValue: 'light',
+  //     onChanged(mode, defaultHandler) {
+  //       if (mode == 'dark') {
+  //         document.documentElement.classList.add('p-dark')
+  //         defaultHandler(mode)
+  //       } else {
+  //         document.documentElement.classList.remove('p-dark')
+  //         defaultHandler(mode)
+  //       }
+  //     }
+  //   })
 
-    watch(darkMode, (newValue) => {
-      colorMode.value = newValue ? 'dark' : 'light'
-    })
+  //   watch(darkMode, (newValue) => {
+  //     colorMode.value = newValue ? 'dark' : 'light'
+  //   })
 
-    onMounted(() => {
-      darkMode.value = colorMode.value === 'dark' || (colorMode.value === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
-    })
-  }
+  //   onMounted(() => {
+  //     darkMode.value = colorMode.value === 'dark' || (colorMode.value === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  //   })
+  // }
 
   return {
     darkMode,

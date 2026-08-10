e6d09b2d365553f4bb0f0e19902dc6a817d31d40
Zadigo <pendenquejohn@gmail.com>
2026-08-11 00:34:07 +0200

    Update packages

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -70,6 +70,9 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
+    firebaseClientEmail: process.env.NUXT_FIREBASE_CLIENT_EMAIL,
+    firebasePrivateKey: process.env.NUXT_FIREBASE_PRIVATE_KEY,
+
     public: {
       // Site
       siteUrl: '',
@@ -94,7 +97,10 @@ export default defineNuxtConfig({
       twilioToPhoneNumber: '',
 
       // Primevue
-      primeVueLicenseKey: process.env.NUXT_PRIME_VUE_LICENSE_KEY
+      primeVueLicenseKey: process.env.NUXT_PRIME_VUE_LICENSE_KEY,
+
+      // Firebase
+      firebaseProjectId: ''
     }
   },
 
@@ -301,14 +307,14 @@ export default defineNuxtConfig({
 
   vuefire: {
     config: {
-      apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      dbUrl: process.env.NUXT_PUBLIC_FIREBASE_DB_URL,
-      storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
-      measurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-      messageSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
-      projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID
+      apiKey: process.env.NUXT_FIREBASE_API_KEY,
+      authDomain: process.env.NUXT_FIREBASE_AUTH_DOMAIN,
+      dbUrl: process.env.NUXT_FIREBASE_DB_URL,
+      storageBucket: process.env.NUXT_FIREBASE_STORAGE_BUCKET,
+      appId: process.env.NUXT_FIREBASE_APP_ID,
+      measurementId: process.env.NUXT_FIREBASE_MEASUREMENT_ID,
+      messageSenderId: process.env.NUXT_FIREBASE_MESSAGE_SENDER_ID,
+      projectId: process.env.NUXT_FIREBASE_PROJECT_ID
     }
   },
 
